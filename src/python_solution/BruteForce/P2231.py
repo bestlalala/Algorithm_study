936ffc1f0e05b22e7588888994f7f819191c9b04
yeonsu <bestdustn@sookmyung.ac.kr>
2023-04-14 21:22:38 +0900

    [백준] 브루트포스&정렬

--- src/python_solution/BruteForce/P2231.py
+++ src/python_solution/BruteForce/P2231.py
@@ -1,1 +1,14 @@
 # 분해합
+
+n = int(input())
+
+for i in range(1, n+1):
+    num = sum(map(int, str(i)))
+    n_sum = i + num
+
+    if n_sum == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
+
