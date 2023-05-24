123693552a3423f62d783e8e451383c6228be55a
yeonsu <bestdustn@sookmyung.ac.kr>
2023-05-25 00:00:17 +0900

    [1로 만들기]

--- src/python_solution/Dynamic_Programming/P1463.py
+++ src/python_solution/Dynamic_Programming/P1463.py
@@ -2,13 +2,14 @@
 
 n = int(input())
 
-x = [0] * (n+1)
+x = [0] * 1000001
 
 for i in range(2, n+1):
     x[i] = x[i-1] + 1
-    if n % 3 == 0:
-        x[i] = min(x[i], x[i//3] + 1)
     if n % 2 == 0:
         x[i] = min(x[i], x[i//2] + 1)
+    if n % 3 == 0:
+        x[i] = min(x[i], x[i//3] + 1)
+
 print(x[n])
 
