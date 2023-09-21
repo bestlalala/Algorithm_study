cd9fa980caa8581c6170fa912ed4256b23736e4c
yeonsu <bestdustn@sookmyung.ac.kr>
2023-09-21 18:36:00 +0900

    [BFS] 2606번 바이러스

---
+++ src/python_solution/BFS/P2606.py
@@ -0,0 +1,27 @@
+# 2606번. 바이러스
+
+n = int(input())
+m = int(input())
+
+adj = [[0]*(n+1) for _ in range(n+1)]
+visited = [0] * (n+1)
+
+global cnt
+cnt = 0
+
+for i in range(m):
+    x, y = map(int, input().split())
+    adj[x][y] = adj[y][x] = 1
+
+
+def bfs(v):
+    visited[v] = 1
+    global cnt
+    for i in range(n+1):
+        if visited[i] == 0 and adj[v][i] == 1:
+            bfs(i)
+            cnt += 1
+
+
+bfs(1)
+print(cnt)
