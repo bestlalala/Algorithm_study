4369d877a65cbdf6c9c965f98a101bec5ee52c12
yeonsu <bestdustn@sookmyung.ac.kr>
2023-10-10 15:37:44 +0900

    [BFS] 9205번 맥주 마시면서 걸어가기

---
+++ src/python_solution/BFS/P9205.py
@@ -0,0 +1,34 @@
+# 9205번. 맥주 마시면서 걸아가기
+from collections import deque
+
+
+def bfs():
+    que = deque()
+    que.append(home)
+    visited = set()
+
+    while que:
+        x, y = que.popleft()
+        if abs(x-festival[0]) + abs(y-festival[1]) <= 1000:
+            print('happy')
+            return
+        for i in range(n):
+            if shop[i] not in visited:
+                nx, ny = shop[i]
+                if abs(x-nx) + abs(y-ny) <= 1000:
+                    visited.add((nx, ny))
+                    que.append((nx, ny))
+    print('sad')
+    return
+
+
+t = int(input())    # 테스트 케이스 개수
+
+for _ in range(t):
+    n = int(input())  # 편의점의 수
+    home = tuple(int(m) for m in input().split())  # 상근이네 집
+    shop = []
+    for _ in range(n):
+        shop.append(tuple(int(m) for m in input().split()))
+    festival = tuple(int(m) for m in input().split())
+    bfs()
