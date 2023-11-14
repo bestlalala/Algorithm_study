8c4bf071a3370e27b494516b50e73eddeee22661
yeonsu <bestdustn@sookmyung.ac.kr>
2023-11-14 10:52:47 +0900

    [stack] 6198번. 옥상 공원 꾸미기

---
+++ src/python_solution/Stack/P6198.py
@@ -0,0 +1,18 @@
+# 6198번 옥상 공원 꾸미기
+
+n = int(input())
+buildings = [int(input()) for i in range(n)]
+
+stack = []
+result = 0
+
+for b in buildings:
+    while stack and stack[-1] < b:
+        stack.pop()
+    stack.append(b)
+    result += len(stack)-1
+
+print(result)
+
+
+
