2c2c043a2944b066d141faaa60aea1c6512702d7
yeonsu <bestdustn@sookmyung.ac.kr>
2023-11-14 10:55:59 +0900

    [fix] 부등호 수정 - 등호 추가

--- src/python_solution/Stack/P6198.py
+++ src/python_solution/Stack/P6198.py
@@ -7,7 +7,7 @@ stack = []
 result = 0
 
 for b in buildings:
-    while stack and stack[-1] < b:
+    while stack and stack[-1] <= b:
         stack.pop()
     stack.append(b)
     result += len(stack)-1
