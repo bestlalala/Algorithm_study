3d9bb40559064de81e65950b93fd53ec0a45b1b0
bestlalala <bestdustn@sookmyung.ac.kr>
2025-03-06 18:16:50 +0900

    [백준] 2606번. 바이러스
    - 코드 개선
    - 인접 리스트 이용하여 메모리 최적화
    - visited 배열을 리스트로 변경
    - 큐에서 꺼낸 후에 인접 노드 탐색

--- python_solution/BFS/P2606.py
+++ python_solution/BFS/P2606.py
@@ -7,32 +7,28 @@ input = sys.stdin.readline
 n = int(input().rstrip())   # 컴퓨터의 수
 m = int(input().rstrip())   # 네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수
 
-coms = collections.defaultdict(set)
-visited = collections.defaultdict(bool)
+coms = collections.defaultdict(list)
+visited = [False] * (n+1)
 
 for _ in range(m):
     a, b = map(int, input().rstrip().split())
     coms[a].add(b)
     coms[b].add(a)
 
-for i in range(n):
-    visited[i+1] = False
-
 def bfs(v):
     que = collections.deque([v])
+    visited[v] = True   # 방문 표시
     cnt = 0
     
     while que:
         v = que.popleft()
-        cnt += 1
-        visited[v] = True
-        
-        while coms[v]:
-            c = coms[v].pop()
-            if not visited[c] and c not in que:
+
+        for c in coms[v]:
+            if not visited[c]:
                 que.append(c)
-                coms[c].remove(v)
+                visited[c] = True
+                cnt += 1    # 감염된 컴퓨터 수 증가
     
     return cnt
 
-print(bfs(1)-1)
+print(bfs(1))
