94a6545a63aa93999fb95797524c8d540da69dc8
bestlalala <bestdustn@sookmyung.ac.kr>
2025-03-07 19:19:32 +0900

    [백준] 2156번. 포도주 시식 (DP)

--- python_solution/Dynamic_Programming/P2156.py
+++ python_solution/Dynamic_Programming/P2156.py
@@ -1,23 +1,27 @@
 # 2156번. 포도주 시식
 
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-wine = [int(input()) for _ in range(n)]
-d = [0]*n
+wine = [int(input().rstrip()) for _ in range(n)]
 
-d[0] = wine[0]
+# dp[i] = k
+# i번째 잔까지 있을 때 마실 수 있는 최대 포도주의 양
 
-if n == 2:
-    d[1] = wine[0] + wine[1]
+dp = [-1] * 10001
 
+if n == 1:
+    dp[1] = wine[0]
+elif n == 2:
+    dp[2] = wine[0] + wine[1]
 elif n == 3:
-    d[1] = wine[0] + wine[1]
-    d[2] = max(d[1], wine[0] + wine[2], wine[1] + wine[2])
-
-elif n > 3:
-    d[1] = wine[0] + wine[1]
-    d[2] = max(d[1], wine[0] + wine[2], wine[1] + wine[2])
-    for i in range(3, n):
-        d[i] = max(d[i-1], d[i-2]+wine[i], d[i-3] + wine[i] + wine[i-1])
-
-print(max(d))
-
+    dp[3] = max(wine[0] + wine[1], wine[1] + wine[2], wine[2] + wine[0])
+else:
+    dp[1] = wine[0]
+    dp[2] = wine[0] + wine[1]
+    dp[3] = max(wine[0] + wine[1], wine[1] + wine[2], wine[2] + wine[0])
+    for i in range(4, n+1):
+        dp[i] = max(dp[i-1], dp[i-2] + wine[i-1], dp[i-3] + wine[i-2] + wine[i-1])
+        
+print(dp[n])
