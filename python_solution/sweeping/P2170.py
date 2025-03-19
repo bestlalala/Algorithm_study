3c7736f3c7539ecc739485154aaacd54efe262cf
bestlalala <bestdustn@sookmyung.ac.kr>
2025-03-19 15:51:12 +0900

    [백준] 2170번. 선 긋기
    - 다른 풀이 추가

--- python_solution/sweeping/P2170.py
+++ python_solution/sweeping/P2170.py
@@ -21,3 +21,22 @@ for i in range(2*n-1):
         length += line[i+1][0] - line[i][0]
 
 print(length)
+
+# 다른 풀이
+# n = int(input())
+# points = [tuple(int(p) for p in input().split()) for _ in range(n)]
+
+# points.sort()
+
+# length = 0
+# last_start = points[0][0]
+# last_end = points[0][1]
+# for x, y in points[1:]:
+#     # 완전히 새로운 선분
+#     if last_end < x:
+#         length += last_end - last_start
+#         last_start = x
+#     last_end = max(last_end, y)
+# length += last_end - last_start
+
+# print(length)
